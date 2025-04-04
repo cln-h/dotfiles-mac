26e93223ce9d78cf3440dc0659fa9290738e4a77
cln-h <cullen.hutchison@gmail.com>
2025-04-04 03:56:22 -0500

    Format on save

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -59,3 +59,20 @@ autocmd("BufEnter", {
   end
 })
 
+-- Format on save
+autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+
+    if client.supports_method('textDocument/formatting') then
+      -- format the current buffer on save
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
+  end,
+})
